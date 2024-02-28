08dc53cb0bb22e14f0064364de67d74193ce31de
Ben Geier <geier.benjamin@gmail.com>
2024-02-28 11:20:19 -0500

    work on about. fix contact.

---
+++ script.js
@@ -0,0 +1,5 @@
+const changeText = document.querySelector("#q1");
+
+changeText.addEventListener("click", function() {
+  changeText.textContent = "I loved writing and telling stories, so journalism was a good fit for me";
+});
